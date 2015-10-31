94463fe89cb572145a282c82ca999f9f4f5ebdf5
oitsjustjose <stovallj1995@gmail.com>
2015-10-31 17:46:43 -0500

    Update Incoming!

--- src/main/java/com/oitsjustjose/criss_cross/Util/Reference.java
+++ src/main/java/com/oitsjustjose/criss_cross/Util/Reference.java
@@ -3,6 +3,6 @@ package com.oitsjustjose.criss_cross.Util;
 public class Reference
 {
 	public static final String modid = "CrissCross";
-	public static final String version = "1.2";
+	public static final String version = "1.3";
 	public static final String guifactory = "com.oitsjustjose.criss_cross.GUI.GUIFactory";
 }
